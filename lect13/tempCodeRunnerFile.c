6c36bb90d276cd9b7cfeb59a6e9280bdc10c2a5e
sahilvish01 <sahilvish6542@gmail.com>
2025-09-22 15:13:49 +0530

    Array

---
+++ lect13/tempCodeRunnerFile.c
@@ -0,0 +1,29 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int a[5]={1,2,3,2,1},i,j,rev[5],count=0;
+//     for(i=0,j=4;i<5;j--,i++)
+//     {
+//         rev[i]=a[j];
+
+//     }
+//     for(count=0,i=0;i<5;i++)
+//     {
+//         if(rev[i]!=a[j])
+//         {
+//             count++;
+//             break;
+//         }
+
+
+//     }
+//     if(count>0)
+//     {
+//         printf("Pelindrome !!");
+//     }
+//     else
+//     {
+//         printf("Pelindrome !!");
+//     }
+
+// }
