0510012df2343e574412e527aae3fbaf3e288cfa
sahilvish01 <sahilvish6542@gmail.com>
2025-10-24 14:36:36 +0530

    Print array through pointer

---
+++ lect17/c76.c
@@ -0,0 +1,12 @@
+// Print array through pointer 
+
+#include<stdio.h>
+int main()
+{
+    int arr[5]={1,2,3,45,6};
+    int *p=arr;
+    for(int i=0;i<5;i++)
+    {
+        printf("\n%d",*(p+i));
+    }
+}
