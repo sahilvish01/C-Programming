b43eb26762fdad8020b42cbdb6bb9da33bf3b9e8
sahilvish01 <sahilvish6542@gmail.com>
2025-10-25 01:56:11 +0530

    Compare two arrays through pointer

---
+++ lect17/c81.c
@@ -0,0 +1,26 @@
+//Compare two arrays through pointer
+
+#include<stdio.h>
+int main()
+{
+    int a[5]={1,2,3,45,6};
+    int b[5]={1,2,3,45,6};
+    int *p = a;
+    int *q = b;
+    int i;
+    for(i=0;i<5;i++)
+    {
+        if(*(p+i)!=*(q+i))
+        {
+            break;
+        }
+    }
+    if(i==5)
+    {
+        printf("Same");
+    }
+    else
+    {
+        printf("Not Same");
+    }
+}
