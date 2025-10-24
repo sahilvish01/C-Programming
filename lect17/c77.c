d7ab229108e36988c47c37d153d9863b709f306b
sahilvish01 <sahilvish6542@gmail.com>
2025-10-24 14:37:28 +0530

    Swap two numbers call by reference

---
+++ lect17/c77.c
@@ -0,0 +1,17 @@
+// Swap two numbers call by reference
+
+#include<stdio.h>
+int swap(int *a,int *b)
+{
+    int t;
+    t=*a;
+    *a=*b;
+    *b=t;
+}
+int main()
+{
+    int a=5;
+    int b=10;
+    swap(&a,&b);
+    printf("a=%d b=%d",a,b);
+}
