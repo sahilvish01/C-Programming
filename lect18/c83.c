c2c373409aebd8299d693e3692e374784cf18cf6
sahilvish01 <sahilvish6542@gmail.com>
2025-10-30 16:01:47 +0530

    Sum of 2d array

---
+++ lect18/c83.c
@@ -0,0 +1,15 @@
+//Sum of 2d array
+
+#include<stdio.h>
+int main()
+{
+    int a[3][3]={{1,2,3},{4,5,6},{7,8,9}},sum=0;
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            sum=sum+a[i][j];
+        }
+    }
+    printf("Sum = %d",sum);
+}
