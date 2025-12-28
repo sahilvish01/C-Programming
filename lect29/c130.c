f498fc77624f7915d71c6665ac275154f435d2b8
sahilvish01 <sahilvish6542@gmail.com>
2025-12-28 16:19:03 +0530

    Left and Right Shift

---
+++ lect29/c130.c
@@ -0,0 +1,16 @@
+// Left and Right Shift
+
+#include<stdio.h>
+int main()
+{
+    int a = 20;
+
+    
+
+    printf("%d\n",a << 1); //left Shift
+    printf("%d\n",a << 2); //left Shift
+
+    printf("%d\n",a >> 1); //Right Shift
+    printf("%d\n",a >> 2); //Right Shift
+
+}
